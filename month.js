749101ce6fbcd3eeb22a9456cdcfb8f45d0091b8
Anaketa <g.sambrotta@gmail.com>
2014-10-01 15:57:55 +0200

    correctly display month view

--- month.js
+++ month.js
@@ -9,25 +9,27 @@ function createCalendar(){
     wca.currentMonth = wca.today.getMonth(); //month in number
     wca.currentDayOfWeek = wca.today.getDay();
     wca.firstDay = new Date(wca.currentYear, wca.currentMonth, 1); //first day of current month
-    wca.firstDayOfWeek = daysLabel[wca.firstDay.getDay()]; // first dayOfTheWeek of current month
+    wca.firstDayOfWeek = daysLabel[wca.firstDay.getDay() - 1]; // first dayOfTheWeek of current month
+    wca.firstDayOfWeekNumber = wca.firstDay.getDay(); 
     wca.showMonth = monthNamesArray[wca.currentMonth]; //current month string
-    wca.showWeekDay = daysLabel[wca.currentDayOfWeek]; //current day of the week
+    wca.showWeekDay = daysLabel[wca.currentDayOfWeek - 1]; //current day of the week
     wca.totDayInMonth = daysPerMonth[wca.currentMonth]; //how many days are in the current month
 
+    // *** DEBUGGER *** //
     console.log("firstDay:", wca.firstDay);
     console.log("firstDayOfWeek:", wca.firstDayOfWeek);
     console.log("monthName", wca.showMonth);
     console.log("daysLabel", wca.showWeekDay);
     console.log("daysPerMonth", wca.totDayInMonth);
+    console.log("firstDayOfWeekNumber", wca.firstDayOfWeekNumber);
 
     //Leap years
-    if (wca.currentMonth == 1) { // February 
+    if (wca.currentMonth == 1) {
         if((wca.currentYear % 4 == 0 && wca.currentYear % 100 != 0) || wca.currentYear % 400 == 0){
           wca.totDayInMonth = 29;
         }
     }
 
-    // Fill in html con dati sopra trovati
     var Htmltable = document.getElementById('table');
     var year = document.getElementById('year');
     var month = document.getElementById('month');
@@ -39,29 +41,30 @@ function createCalendar(){
     for(i = 0; i < daysLabel.length; i++)
         daysOfWeek.innerHTML = daysOfWeek.innerHTML + '<td>'+ daysLabel[i] +'</td>';
 
+
+    
+    var html = '';
     var day = 1;
-    if(day <= wca.totDayInMonth){
-        Htmltable.innerHTML += '<tr id="days-row"></tr>';
-        
-        for(k = 0; k <= 6; k++){
-            var daysRow = document.getElementById('days-row');
-            daysRow.innerHTML = daysRow.innerHTML + '<td>' + day + '</td>';
-            day++;
+    for(i = 1; i <= 6; i++){ //6 rows
+        html += '<tr id="days-row">';
+        for(j = 1; j <= 7; j++){ //7 coloumns
+            html += '<td>'; 
+            if(day <= wca.totDayInMonth && (i > 1 || j >= wca.firstDayOfWeekNumber )){
+                html += day;
+                day++;
+            } else {
+                html += '&nbsp;';
+            }
+            html += '</td>';
         }
-    }
+        if(day >= wca.totDayInMonth){
+            break;
+        }
+        html += '</tr>'; 
+    }  
+    Htmltable.innerHTML += html + '</tr>';
 
 }
-
-
-days = function(){
-//Array with all the day of the current month
-    days = [];
-    for(i = 1; i <= wca.totDayInMonth; i++){
-       days.push(i);
-    }
-    return days;
-}
-
 
 
 })();
